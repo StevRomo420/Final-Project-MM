e95b15d5b5f651cca478c16cff65ec05363bb1f4
Cletoman <Cristeban76@gmail.com>
2022-07-10 20:27:26 -0600

    En la vista de detalle la pelicula se agrega la funcionalidad de reproducir el video provisto por el API de TMDB

--- js/detail.js
+++ js/detail.js
@@ -1,22 +1,35 @@
+/**
+ * @author Esteban Taisigue B97785
+ * @author Erick Vasquez 	B98334
+ * @author Esteban Rosales 	B96967
+ * 
+ */
+
 document.addEventListener('DOMContentLoaded',function(){
 
 	//PARAM
-	const params 		= new URLSearchParams(window.location.search);
+	const params 			= new URLSearchParams(window.location.search);
 
 	//COMPONENT
-	const posterImg 	= $('.poster-img-js');
-	const bigPosterImg 	= $('.big-poster-img-js');
-	const backImg 		= $('.back-img-js');
-	const title			= $('.movie-title-js');
-	const length		= $('.movie-length-js');
-	const genre			= $('.movie-genre-js');
-	const rating		= $('.movie-rating-js');
-	const starRating	= $('.stars-js');
-	const description	= $('.movie-description-js');
-	const movie			= $('.movie-js');
-	const warning		= $('.warning-js');
-	const errMsg		= $('.error-msg-js');
-	const carousel		= $('.carousel-js');
+	const posterImg 		= $('.poster-img-js');
+	const bigPosterImg 		= $('.big-poster-img-js');
+	const backImg 			= $('.back-img-js');
+	const title				= $('.movie-title-js');
+	const length			= $('.movie-length-js');
+	const genre				= $('.movie-genre-js');
+	const rating			= $('.movie-rating-js');
+	const starRating		= $('.stars-js');
+	const description		= $('.movie-description-js');
+	const premiereDate		= $('.premiere-date-js')
+	const movie				= $('.movie-js');
+	const warning			= $('.warning-js');
+	const errMsg			= $('.error-msg-js');
+	const carousel			= $('.carousel-js');
+	const videoIframe		= $('.video-iframe-js');
+
+	const youtubeReplaceKey = 'YT_KEY';
+	let youtubeEmbedPath 	= `https://www.youtube.com/embed/${youtubeReplaceKey}?autoplay=1`;
+	let moviePath 			= masterUrl.concat('movie/').concat(replaceKey).concat('/videos').concat(apiKeyAndLanguageParameter);
 
 
 	//INITAL MOVIE FIND CALL
@@ -52,18 +65,82 @@ document.addEventListener('DOMContentLoaded',function(){
 		backImg.attr('src',imagePath.replace(replaceImageSizeKey,'original').replace(replaceImagePathKey,data.backdrop_path));
 		title.text(data.title);
 		length.text(data.runtime);
+		description.text(data.overview);
+		rating.text(data.vote_average);
+		premiereDate.text(data.release_date);
 
+		showGenresTag(data.genres);
+		showStarRating(data.vote_average);
+		showCompanies(data.production_companies);
+		findVideo(data.id);
+
+		createEventListeners();
+
+	}
+
+
+	function findVideo(id){
+		
+		apiRequest({
+			url:moviePath.replace(replaceKey,id),
+			onSuccess:function(data){
+				const result=data.results;
+				if(result.length>0){
+					if(result[0].site=='YouTube'){
+						
+						videoIframe.data('video',result[0].key);
+						createPLayVideoEventListeners();
+					}
+				}
+			},
+			onFail:function(xhr,status,error){
+				//Pass
+			}
+		})
+
+	}
+
+
+
+	function showGenresTag(genres){
+		
 		let genresTag ='';
-		data.genres.forEach(function(genreItem){
+		genres.forEach(function(genreItem){
 			genresTag = genresTag.concat(genreItem.name).concat(',');
 		});
 		genresTag=genresTag.slice(0,-1);
 		genre.text(genresTag);
+	
+	}
 
-		description.text(data.overview);
-		rating.text(data.vote_average);
+	function showStarRating(vote_average){
 
-		data.production_companies.forEach(function(company){
+		const maxStars = 5;
+		let voteStars = (Math.round(vote_average)/2);
+
+		for (let i = 0; i<maxStars; i++) {
+
+			let classTag='';
+			
+			if(voteStars>=1){
+				classTag='fa fa-star';
+			}else if((voteStars<1) && (voteStars>0)){
+				classTag='fa fa-star-half-o';
+			}else{
+				classTag='fa fa-star-o';
+			}
+
+			voteStars--;
+
+			$('<span>',{class:classTag}).appendTo(starRating);
+		}
+
+	}
+
+
+	function showCompanies(production_companies){
+		
+		production_companies.forEach(function(company){
 		
 			if(company.logo_path!=null){
 				
@@ -88,32 +165,26 @@ document.addEventListener('DOMContentLoaded',function(){
 
 		});
 
-		const maxStars = 5;
-		let voteStars = (Math.round(data.vote_average)/2);
+	}
 
-		for (let i = 0; i<maxStars; i++) {
+	function createPLayVideoEventListeners(){
+		
+		if(videoIframe.data('video')){
 
-			let classTag='';
-			
-			if(voteStars>=1){
-				classTag='fa fa-star';
-			}else if((voteStars<1) && (voteStars>0)){
-				classTag='fa fa-star-half-o';
-			}else{
-				classTag='fa fa-star-o';
-			}
+			$('.play-btn-js').click(function(){	
+				videoIframe.attr('src',youtubeEmbedPath.replace(youtubeReplaceKey,videoIframe.data('video')));
+				$('.video-box-js').toggleClass('hidden');
+			});
 
-			voteStars--;
-
-			$('<span>',{class:classTag}).appendTo(starRating);
+			$('.close-video-box-btn-js').click(function(){
+				$('.video-box-js').toggleClass('hidden');
+				videoIframe.attr('src','');
+			});
 		}
-
-		creteEventeListeners();
 
 	}
 
-
-	function creteEventeListeners(){
+	function createEventListeners(){
 		
 		posterImg.hover(function(){
 			$('.big-poster-js').toggleClass('hidden');
@@ -124,16 +195,7 @@ document.addEventListener('DOMContentLoaded',function(){
 			$('.background-box-js').toggleClass('md:blur-sm');
 			backImg.toggleClass('scale-medium');
 		});
-
-		$('.play-btn-js').click(function(){
-			$('.video-box-js').toggleClass('hidden');
-			$(window).scrollTop($('.video-box-js').position().top);
-		});
-
-		$('.close-video-box-btn-js').click(function(){
-			$('.video-box-js').toggleClass('hidden');
-		});
-
+	
 	}
 
 
