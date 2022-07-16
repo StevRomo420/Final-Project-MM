aa5c5a06f7779eb68e6f055880d89ed7d31e49fa
Cletoman <Cristeban76@gmail.com>
2022-07-15 21:29:18 -0600

    Se modifica el link en el poster desplegado para cada pelicula

--- js/index.js
+++ js/index.js
@@ -134,7 +134,7 @@ document.addEventListener('DOMContentLoaded',function(){
 		                            <span class="rounded-full relative bg-teal-300 p-3 text-white self-end right-[-25px]">${movie.vote_average}</span>
 		                        </div>
 		                        <div class="overlap-item z-20 self-center flex justify-center">
-		                            <a href="view/detail.html${eachMovieDetailLink.replace(replaceKey,movie.id)}" class="hover:animate-bounce"> 
+		                            <a href="${eachMovieDetailLink.replace(replaceKey,movie.id)}" class="hover:animate-bounce"> 
 		                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inside-svg" viewBox="0 0 16 16">
 		                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
 		                                    <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
