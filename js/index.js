7a0dd943713ce3307bd1572eecd14cb2f2b516b9
Cletoman <Cristeban76@gmail.com>
2022-07-17 21:21:31 -0600

    Se agrega la funcionalidad de modo oscuro en la vista index

--- js/index.js
+++ js/index.js
@@ -236,8 +236,42 @@ document.addEventListener('DOMContentLoaded',function(){
 		});
 	}
 
+	function storageOperations(type){
+		
+		try{
 
+			const root = document.getElementsByTagName('html')[0];
+			let mode = localStorage.getItem('darkThemeMode');
+
+			if(mode==null){mode=false;localStorage.setItem('darkThemeMode',mode);}
+
+			if((type) && (mode=='true')){
+				root.classList.toggle('dark');
+
+			}else if(!type){
+				root.classList.toggle('dark');
+				localStorage.setItem('darkThemeMode',(mode=='false')?true:false);
+			}
+
+			document.body.classList.add('transition-colors','duration-800');
+
+
+		}catch(error){
+
+		}
+
+	}
+
+	function createEventListeners(){
+		$('.toggle-theme-js').click(function(){
+			storageOperations(false);
+		});
+	}
+
+	storageOperations(true);
 	loadGenres();
 	beginOfIndex();
+	createEventListeners();
+
 	
 });
